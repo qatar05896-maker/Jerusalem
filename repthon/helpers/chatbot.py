60c80346995c9aad6a5fec6f4b5c4b335bb18429
qatar05896-maker <qatar05896@gmail.com>
2026-04-28 13:05:19 +0300

    Update chatbot.py

--- repthon/helpers/chatbot.py
+++ repthon/helpers/chatbot.py
@@ -1,11 +1,4 @@
-from .utils.extdl import install_pip
-
-try:
-    import randomstuff
-except ModuleNotFoundError:
-    install_pip("randomstuff.py")
-    import randomstuff
-
 from ..Config import Config
 
-rs_client = randomstuff.AsyncClient(api_key=Config.RANDOM_STUFF_API_KEY, version="4")
+# تم تعطيل المكتبة لمنع الكراش
+rs_client = None
